a24ea67e914d6438ce1f81cd42ee2d86b2c9f3e6
sebastian <sebastian.gebala@gmail.com>
2016-09-27 20:32:28 +0200

    button

--- static/jquery/centrum.js
+++ static/jquery/centrum.js
@@ -1,1 +1,21 @@
-jquery
+$(document).ready(function() {
+	$('.pay_pal').mouseenter(function() {
+		$(this).addClass("pay_opa");
+	});
+});
+$(document).ready(function() {
+	$('.pay_pal').mouseleave(function() {
+		$(this).removeClass("pay_opa");
+	});
+});
+
+$(document).ready(function() {
+	$('.content_4').mouseenter(function() {
+		$(this).addClass("pay_opa");
+	});
+});
+$(document).ready(function() {
+	$('.content_4').mouseleave(function() {
+		$(this).removeClass("pay_opa");
+	});
+});
